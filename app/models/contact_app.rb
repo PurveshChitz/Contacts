53a7716464c78014a88629f8c31a824b370b3d00
Purvesh <pchithore@gmail.com>
2017-01-07 11:57:04 +0530

    First commit

--- app/models/contact_app.rb
+++ app/models/contact_app.rb
@@ -3,8 +3,12 @@ class ContactApp < ActiveRecord::Base
 	validates :Phone,:presence => true,
                  :numericality => true,
                  :length => { :minimum => 10, :maximum => 15 }
+    validates :Work_Phone,:allow_blank=>true,
+                 :numericality => true,
+                 :length => { :minimum => 10, :maximum => 15 }
     validates :Telephone,:allow_blank=>true,  :numericality => true,
                  :length => { :minimum => 10, :maximum => 15 }
     validates_format_of :Email_Id,:allow_blank=>true, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+    validates_format_of :Work_Email,:allow_blank=>true, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
     
 end
